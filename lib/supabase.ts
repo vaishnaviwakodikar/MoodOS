b7cffe5a8d64fb53d7d3cf7d54c58fb677282711
vaishnaviwakodikar <vaishnaviwakodikar09@gmail.com>
2026-06-07 02:02:52 +0530

    ENV updated

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,7 +3,7 @@ import type { Database } from "@/types/database";
 
 export function createClient() {
   return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    "https://axjijxvbxnamqsrxurlp.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4amlqeHZieG5hbXFzcnh1cmxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4ODY0NzUsImV4cCI6MjA5NDQ2MjQ3NX0.jjZmavwXf0_rEJfqRVhMnhjaMFrTEejn8T20149w3wU"
   );
 }
