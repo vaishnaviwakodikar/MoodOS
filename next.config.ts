2c5e25f0ceeca40959630a875d722768c3cf92b8
vaishnaviwakodikar <vaishnaviwakodikar09@gmail.com>
2026-06-27 10:43:48 +0530

    Split files

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      enabled: false,
-    },
-  },
+  turbopack: false,
 };
 
 export default nextConfig;
